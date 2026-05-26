be48fdf44b2391734ed55c4b4343725c04ddcdf3
zhangqizhe <64081029+qzzhang0131@users.noreply.github.com>
2026-05-26 15:31:11 +0800

    test

--- src/utils/prediction_utils.py
+++ src/utils/prediction_utils.py
@@ -2,13 +2,13 @@ import numpy as np
 
 def return_first_last_predictions(all_predictions: np.ndarray, seq_len: int = 16384, 
                             offset_len: int = 0) -> (np.ndarray, np.ndarray):
-    hf_offset_len = offset_len // 4
+    hf_offset_len = offset_len // 2
     
     return all_predictions[0, :, :seq_len - hf_offset_len], all_predictions[-1, :, hf_offset_len:]
 
 def return_first_other_last_predictions(all_predictions: np.ndarray, seq_len: int = 16384, 
                             offset_len: int = 0) -> (np.ndarray, np.ndarray, np.ndarray):
-    hf_offset_len = offset_len // 4
+    hf_offset_len = offset_len // 2
     
     return all_predictions[0, :, :seq_len - hf_offset_len], all_predictions[1:-1, :, hf_offset_len:seq_len - hf_offset_len], all_predictions[-1, :, hf_offset_len:]
 
@@ -16,21 +16,16 @@ def combined_predictions(first_prediction: np.ndarray, last_prediction: np.ndarr
     first_last_seq_len = first_prediction.shape[-1]
     tail = chrom_len - first_last_seq_len
     predictions = np.zeros((6, chrom_len), dtype=np.float16)
-    predictions[:, :first_last_seq_len] = first_prediction
     
-    predictions[:, chrom_len - tail:chrom_len] = last_prediction[:, first_last_seq_len - tail:first_last_seq_len]
-    predictions[:, chrom_len - first_last_seq_len:chrom_len - tail] = 0.5 * (
-        predictions[:, chrom_len - first_last_seq_len:chrom_len - tail] + 
-        last_prediction[:, :first_last_seq_len - tail]
-    )
+    predictions[:, :first_last_seq_len] = first_prediction
+    predictions[:, first_last_seq_len:] = last_prediction[:, first_last_seq_len - tail:first_last_seq_len]
     
     return predictions
     
 def combined_overlap_predictions(first_prediction: np.ndarray, last_prediction: np.ndarray, other_predictions: np.ndarray,
-                num_valid_examples: int, chrom_len: int, offset_len: int = 0) -> np.ndarray:
-    offset_len = offset_len // 2
+                num_valid_examples: int, chrom_len: int) -> np.ndarray:
     first_last_seq_len = first_prediction.shape[-1]
-    res = other_predictions.shape[-1] - offset_len
+    res = other_predictions.shape[-1]
     l = first_last_seq_len + (num_valid_examples - 1) * res
     tail = chrom_len - l
     
@@ -39,16 +34,8 @@ def combined_overlap_predictions(first_prediction: np.ndarray, last_prediction: 
     
     for k in range(1, num_valid_examples):
         current_len = first_last_seq_len + (k - 1) * res
-        predictions[:, current_len - offset_len:current_len] = 0.5 * (
-            predictions[:, current_len - offset_len:current_len] + 
-            other_predictions[k - 1, :, :offset_len]
-        )
-        predictions[:, current_len:current_len + res] = other_predictions[k - 1, :, offset_len:]
+        predictions[:, current_len:current_len + res] = other_predictions[k - 1, ...]
     
     predictions[:, chrom_len - tail:chrom_len] = last_prediction[:, first_last_seq_len - tail:first_last_seq_len]
-    predictions[:, chrom_len - first_last_seq_len:chrom_len - tail] = 0.5 * (
-        predictions[:, chrom_len - first_last_seq_len:chrom_len - tail] + 
-        last_prediction[:, :first_last_seq_len - tail]
-    )
     
     return predictions
